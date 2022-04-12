45417af7fac2eb296a54c5c48c9c881b838f0b7d
Raul Comeros, Jr <90462635+noice-noise@users.noreply.github.com>
2022-04-12 21:43:38 +0800

    feat(department): add validator in department name property

--- noisenoice-springboot-learn/src/main/java/com/noicenoise/noisenoicespringbootlearn/entity/Department.java
+++ noisenoice-springboot-learn/src/main/java/com/noicenoise/noisenoicespringbootlearn/entity/Department.java
@@ -1,15 +1,23 @@
 package com.noicenoise.noisenoicespringbootlearn.entity;
 
+import org.hibernate.validator.constraints.Length;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
 
 @Entity
 public class Department {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long departmentId;
+
+    @NotBlank(message = "Please Add Department Name")
+    @Length(min = 1, max = 50)
+    @Valid
     private String departmentName;
     private String departmentAddress;
     private String departmentCode;
